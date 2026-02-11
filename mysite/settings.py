e8f1e0773aa29eb9344ff06a74f9c7596bc6eebd
wagha-builds <arwaghmode09@gmail.com>
2026-02-11 22:37:53 +0530

    Sending Confirmation Emails.

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,3 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'arwaghmode09@gmail.com'
+EMAIL_HOST_PASSWORD = 'spknjcbegnrmtodt'
